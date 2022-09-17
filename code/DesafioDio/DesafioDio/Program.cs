33295cfef31be4f2bbbcaa8d8a11edc757c0c598
Paiva <danpaiva@live.com>
2022-09-17 09:01:46 -0300

    feat: Add primeiro desafio

---
+++ code/DesafioDio/DesafioDio/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int multiplication = 1, sum = 0;
+        string number = Console.ReadLine();
+        char[] arrayNumber = number.ToCharArray();
+        int counter = 0;
+        foreach (var item in arrayNumber)
+        {
+            multiplication *= int.Parse(arrayNumber.GetValue(counter).ToString());
+            sum += int.Parse(arrayNumber.GetValue(counter).ToString());
+            counter++;
+        }
+
+        Console.WriteLine(multiplication - sum);
+    }
+}
