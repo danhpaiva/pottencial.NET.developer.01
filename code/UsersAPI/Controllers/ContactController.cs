0ae8c44bb8b069ad564871b39bcc781d22c50c48
Paiva <danpaiva@live.com>
2022-10-01 12:31:39 -0300

    feat: Adicionado método GetById

--- code/UsersAPI/Controllers/ContactController.cs
+++ code/UsersAPI/Controllers/ContactController.cs
@@ -4,27 +4,36 @@ using UsersAPI.Entities;
 
 namespace UsersAPI.Controllers
 {
-  [ApiController]
-  [Route("[controller]")]
-  public class ContactController : ControllerBase
-  {
-    private readonly ScheduleContext _context;
-    public ContactController(ScheduleContext context)
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactController : ControllerBase
     {
-      _context = context;
-    }
+        private readonly ScheduleContext _context;
+        public ContactController(ScheduleContext context)
+        {
+            _context = context;
+        }
 
-    [HttpPost("Create")]
-    public IActionResult Create(Contact contact)
-    {
-      if (string.IsNullOrEmpty(contact.Name))
-        return BadRequest("Todos os campos devem vir preenchidos.");
-      else
-      {
-        _context.Add(contact);
-        _context.SaveChanges();
-        return Ok(contact);
-      }
+        [HttpPost("Create")]
+        public IActionResult Create(Contact contact)
+        {
+            if (string.IsNullOrEmpty(contact.Name))
+                return BadRequest("Todos os campos devem vir preenchidos.");
+            else
+            {
+                _context.Add(contact);
+                _context.SaveChanges();
+                return Ok(contact);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var contact = _context.Contacts.Find(id);
+
+            if (contact == null) return NotFound();
+            return Ok(contact);
+        }
     }
-  }
 }
